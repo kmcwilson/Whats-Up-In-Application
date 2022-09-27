cc5a9757896985ba42cd087fe598587bac0a8881
Katie Wilson <kmcwilson4@gmail.com>
2022-09-26 20:31:19 -0400

    added to the javascript for second page

--- assets/javascript/second.js
+++ assets/javascript/second.js
@@ -1,5 +1,101 @@
 
+const cityInput= document.getElementById('input-city');
+const searchBtn=document.getElementById('search-btn');
+const cityName=cityInput.value.trim();
+//GETTING EVENTS FUNCTION IN LIST
+function getEvents(cityName){
+    let eventUrl= `https://app.ticketmaster.com/discovery/v2/events.json?size=4&city=${cityName}&apikey=2xNO6r6cdtVrFZ7W6Hi5KIVTf2YQsmhQ`;
 
-function getEvents(){
-    const eventUrl= ``
+    fetch(eventUrl)
+
+    .then(function(response){
+        return response.json();
+    })
+
+    .then(function(data){
+     console.log(data)
+    //  let eventSection=document.getElementById('');
+    //  let eventsDisplay= document.createElement('p');
+    //  eventsDisplay.classList.add('events');
+    //  eventsDisplay.innerHTML(data);
+    //  eventSection.appendChild(eventsDisplay);
+})
+} 
+//GETTING THE WEATHER FOR THE CITY
+function getCoordinates(cityName){
+    const requestUrl= `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=21b94d9f597cdce5a2ddff64c6b85a82
+    `;
+    fetch(requestUrl)
+    
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        let lat= data[0].lat;
+        let lon=data[0].lon;
+        console.log(data);
+    searchWeather(lat, lon);
+    })
+    };
+    
+    function searchWeather(lat, lon){
+    let secondUrl=  `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=21b94d9f597cdce5a2ddff64c6b85a82
+    `;
+    fetch(secondUrl)
+    
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+       for(let i=0; i<data.list.length; i+=20){
+            console.log(data.list[i]);   
+    
+            }
+        })
+     }
+
+searchBtn.addEventListener('click', function(){
+   const cityName=cityInput.value.trim();
+   if (!cityName){
+    alert('Error');
+    //TODO display a nice message instead
+    return;
+   }
+       // storeCities(cityName);
+       getEvents(cityName);
+       getCoordinates(cityName);
+       getNews(cityName);
+       
+    //    getNews(cityName);
+   })
+
+function getNews(cityName){
+    let newsUrl= `https://newsapi.org/v2/top-headlines?q=${cityName}&from=2022-09-01&sortBy=popularity&pageSize=4&language=en,fr&apiKey=61e1895e2972426480dd0d5bc3f32a8b`;
+    fetch(newsUrl)
+
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    })
+    
 }
+
+// ''.addEventListener('click', function(){
+//     let originalHeader=document.getElementById('');
+//     let newCity= document.createElement('div');
+//     newCity.innerHTML(cityName);
+//     originalHeader.appendChild(newCity);
+    
+//     getEvents(cityName);
+// })
+
+// function storeCities(cityName){
+//     let cities=[];
+//     let pastCities=document.querySelector('.')
+//     cities.push(cityName);
+//     localStorage.setItem(cities);
+// let cityList= document.querySelector('');
+// }
