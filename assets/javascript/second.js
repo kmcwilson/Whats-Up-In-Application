c684d02674758fc50fe5d2f3aa2fe80f7469111e
Katie Wilson <kmcwilson4@gmail.com>
2022-09-27 21:54:07 -0400

    got the weather to show in the bar

--- assets/javascript/second.js
+++ assets/javascript/second.js
@@ -1,9 +1,12 @@
 const city = decodeURI(document.location.search.split('=')[1]);
-const searchBtn = document.querySelector('search-btn');
-
+const searchButton = document.querySelector('.search-button');
+const weatherBar = document.getElementById('weather');
+const eventBox = document.getElementById('events');
+const newsBox = document.getElementById('hotels');
+const cityTitle = document.getElementById('city-header');
 
 if (city) {
-    let cityTitle = document.getElementById('city-header');
+    // let cityTitle = document.getElementById('city-header');
     cityTitle.innerHTML = '...  ' + city;
     getEvents(city);
     getCoordinates(city);
@@ -25,13 +28,12 @@ if (city) {
     }
 
     function displayEvents(data) {
-        let eventBox = document.getElementById('events');
         for (i = 0; i < data.length; i++) {
             let eventsDisplay = document.createElement('li');
-            let eventLink= document.createElement('a');
-            eventLink.href=data[i].url;
-            eventLink.textContent= data[i].name;
-            eventLink.setAttribute('target','_blank');
+            let eventLink = document.createElement('a');
+            eventLink.href = data[i].url;
+            eventLink.textContent = data[i].name;
+            eventLink.setAttribute('target', '_blank');
             eventsDisplay.classList.add('eventsList');
             eventsDisplay.appendChild(eventLink);
             eventBox.appendChild(eventsDisplay);
@@ -69,85 +71,66 @@ if (city) {
     }
 
     function displayWeather(data) {
-        let weatherBar = document.getElementById('weather');
-        for(let i=0; i<data.length; i++){
-        let currentWeather = document.createElement('li');
-        currentWeather.classList.add('weather-bar');
-        currentWeather.textContent = data[i].name;
-        weatherBar.appendChild(currentWeather);
-    }
-    }
+       debugger;
+            let currentWeather = document.createElement('li');
+            currentWeather.classList.add('weather-bar');
+            currentWeather.textContent = data.main.temp;
+            weatherBar.appendChild(currentWeather);
+        }
 
-    // searchBtn.addEventListener('click', function(){
-    //    const cityName=cityInput.value.trim();
-
-    //    if (!cityName){
-    //     alert('Error');
-    //     //TODO display a nice message instead
-    //     return;
-    //    } currentWeather  
-    
-    // storeCities(cityName);
-    //        getEvents(cityName);
-    //        getCoordinates(cityName);
-    //        getNews(cityName);       
-    //     //    getBars(cityName);
-    //    })
-
-    function getNews(cityName){
-        let newsUrl= `http://api.mediastack.com/v1/news?access_key=3a02bff89377038110c51afa9a144173&languages=en&keywords=${cityName}&limit=4`;
+    function getNews(cityName) {
+        let newsUrl = `http://api.mediastack.com/v1/news?access_key=3a02bff89377038110c51afa9a144173&languages=en&keywords=${cityName}&limit=4`;
         fetch(newsUrl)
 
-        .then(function(response){
-            return response.json();
-        })
-        .then(function(data){
-            console.log(data);
-            displayNews(data.data);
-        })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+                displayNews(data.data);
+            })
 
     }
 
     function displayNews(data) {
         console.log('data:', data);
-        let newsBox = document.getElementById('hotels');
         for (i = 0; i < data.length; i++) {
             let newsDisplay = document.createElement('li');
-            let newsLink= document.createElement('a');
-            newsLink.href=data[i].url;
-            newsLink.textContent= data[i].title;
-            newsLink.setAttribute('target','_blank');
+            let newsLink = document.createElement('a');
+            newsLink.href = data[i].url;
+            newsLink.textContent = data[i].title;
+            newsLink.setAttribute('target', '_blank');
             newsDisplay.classList.add('newsList');
             newsDisplay.appendChild(newsLink);
             newsBox.appendChild(newsDisplay);
         }
     };
 
-    
-    // function getBars(cityName){
-    //     let barsUrl= `https://api.openbrewerydb.org/breweries?per_page=5&by_city=${cityName}`;
-    //     fetch(barsUrl)
+    searchButton.addEventListener('click', function (event) {
+        event.preventDefault();
+        const cityInput = document.getElementById('search-input')
+        const cityName = cityInput.value.trim();
 
-    //     .then(function(response){
-    //         return response.json();
-    //     })
-    //     .then(function(data){
-    //         console.log(data)
-    //     })
 
-    // }
+        if (!cityName) {
+            alert('Error');
+            //TODO display a nice message instead
+            return;
+        }
+        if (cityName) {
+            weatherBar.children.textContent = '';
+            eventBox.children.textContent = '';
+            newsBox.children.textContent = '';
+            cityTitle.textContent = '';
+        }
 
-    
-    
+        cityTitle.innerHTML = '...  ' + city;
+        //   storeCities(cityName);
+        getEvents(cityName);
+        getCoordinates(cityName);
+        getNews(cityName);
+    })
 
-    // ''.addEventListener('click', function(){
-    //     let originalHeader=document.getElementById('');
-    //     let newCity= document.createElement('div');
-    //     newCity.innerHTML(cityName);
-    //     originalHeader.appendChild(newCity);
-
-    //     getEvents(cityName);
-    // })
 
     // function storeCities(cityName){
     //     let cities=[];
