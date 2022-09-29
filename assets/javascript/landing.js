8de96e9aa78ab19840150bd929a729168cc327e0
ryanparketh <ryanpark@rocketmail.com>
2022-09-28 21:58:25 -0400

    added comments

--- assets/javascript/landing.js
+++ assets/javascript/landing.js
@@ -7,8 +7,10 @@ const message = document.getElementById("error");
 searchBtn.addEventListener('click', function(event){
     event.preventDefault();
     const cityName=cityInput.value.trim();
+    //add in no input error message without an alert using bootstrap
     if (!cityName){
      alert('Error');
+    
      //TODO display a nice message instead
 
      return;
